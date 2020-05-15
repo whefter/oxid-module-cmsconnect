f26dd436cf95f135929c6fc3a954162bf9108953
whefter <william@whefter.de>
2020-05-15 13:08:15 +0200

    fix(oxid-6.2): adjustments to work with OXID 6.2 installation procedure

--- Application/CmsConnect.php
+++ Application/CmsConnect.php
@@ -1,14 +1,10 @@
 <?php
-/**
- * @author      William Hefter <william@whefter.de>
- * @link        http://www.whefter.de
- * @copyright   2014-2017 William Hefter
- */
-
 namespace wh\CmsConnect\Application;
 
-use \wh\CmsConnect\Application\Models\CmsPage;
-use \wh\CmsConnect\Application\Utils as CMSc_Utils;
+use wh\CmsConnect\Application\Models\CmsPage;
+use wh\CmsConnect\Application\Utils as CMSc_Utils;
+use wh\CmsConnect\Application\Models\Cache;
+use wh\CmsConnect\Application\Models\SessionCache;
 
 /**
  * cmsconnect
@@ -24,6 +20,11 @@ use \wh\CmsConnect\Application\Utils as CMSc_Utils;
  */
 class CmsConnect
 {
+    function __construct()
+    {
+        static::init();
+    }
+
     /**
      * Returns the processed text of the requested content on the requested CMS
      * page and the requested OXID language ID
@@ -34,17 +35,17 @@ class CmsConnect
      *
      * @return string
      */
-    public function getContent ($sContentIdent, $sCmsPagePath = null, $sLang = null)
+    public function getContent($sContentIdent, $sCmsPagePath = null, $sLang = null)
     {
-        if ( $sCmsPagePath === null ) {
+        if ($sCmsPagePath === null) {
             $oCmsPage = new CmsPage\Path\Implicit($sLang);
         } else {
             $oCmsPage = new CmsPage\Path($sCmsPagePath, $sLang);
         }
-        
+
         return $oCmsPage->getContent($sContentIdent);
     }
-    
+
     /**
      * Returns the processed text of the requested content on the requested CMS
      * page and the requested OXID language ID
@@ -55,13 +56,13 @@ class CmsConnect
      *
      * @return string
      */
-    public function getContentById ($sContentIdent, $sCmsPageId, $sLang = null)
+    public function getContentById($sContentIdent, $sCmsPageId, $sLang = null)
     {
         $oCmsPage = new CmsPage\Id($sCmsPageId, $sLang);
-        
+
         return $oCmsPage->getContent($sContentIdent);
     }
-    
+
     /**
      * Returns an array containing the text content of all content nodes for the requested CMS page and OXID language ID.
      * Specific nodes can be returned by passing the last argument.
@@ -72,9 +73,9 @@ class CmsConnect
      *
      * @return string[]
      */
-    public function getContentArray ($sCmsPagePath = null, $sLang = null, $aContentIdents = [])
+    public function getContentArray($sCmsPagePath = null, $sLang = null, $aContentIdents = [])
     {
-        if ( $sCmsPagePath === null ) {
+        if ($sCmsPagePath === null) {
             $oCmsPage = new CmsPage\Path\Implicit($sLang);
         } else {
             $oCmsPage = new CmsPage\Path($sCmsPagePath, $sLang);
@@ -82,7 +83,7 @@ class CmsConnect
 
         return $oCmsPage->getContentArray($aContentIdents);
     }
-    
+
     /**
      * Returns an array containing the text content of all content nodes for the requested CMS page and OXID language ID.
      * Specific nodes can be returned by passing the last argument.
@@ -93,13 +94,13 @@ class CmsConnect
      *
      * @return string[]
      */
-    public function getContentArrayById ($sCmsPageId, $sLang = null, $aContentIdents = [])
+    public function getContentArrayById($sCmsPageId, $sLang = null, $aContentIdents = [])
     {
         $oCmsPage = new CmsPage\Id($sCmsPageId, $sLang);
-        
+
         return $oCmsPage->getContentArray($aContentIdents);
     }
-    
+
     /**
      * Returns the full XML object for the requested CMS page and OXID language ID.
      *
@@ -108,17 +109,17 @@ class CmsConnect
      *
      * @return \SimpleXMLElement
      */
-    public function getXml ($sCmsPagePath = null, $sLang = null)
+    public function getXml($sCmsPagePath = null, $sLang = null)
     {
-        if ( $sCmsPagePath === null ) {
+        if ($sCmsPagePath === null) {
             $oCmsPage = new CmsPage\Path\Implicit($sLang);
         } else {
             $oCmsPage = new CmsPage\Path($sCmsPagePath, $sLang);
         }
-        
+
         return $oCmsPage->getXml();
     }
-    
+
     /**
      * Returns the full XML object for the requested CMS page and OXID language ID.
      *
@@ -127,13 +128,13 @@ class CmsConnect
      *
      * @return \SimpleXMLElement
      */
-    public function getXmlById ($sCmsPageId, $sLang = null)
+    public function getXmlById($sCmsPageId, $sLang = null)
     {
         $oCmsPage = new CmsPage\Id($sCmsPageId, $sLang);
-        
+
         return $oCmsPage->getXml();
     }
-    
+
     /**
      * Returns an XML object for the requested content, CMS page and OXID language ID
      *
@@ -143,17 +144,17 @@ class CmsConnect
      *
      * @return \SimpleXMLElement
      */
-    public function getContentXml ($sContentIdent = null, $sCmsPagePath = null, $sLang = null)
+    public function getContentXml($sContentIdent = null, $sCmsPagePath = null, $sLang = null)
     {
-        if ( $sCmsPagePath === null ) {
+        if ($sCmsPagePath === null) {
             $oCmsPage = new CmsPage\Path\Implicit($sLang);
         } else {
             $oCmsPage = new CmsPage\Path($sCmsPagePath, $sLang);
         }
-        
+
         return $oCmsPage->getContentXml($sContentIdent);
     }
-    
+
     /**
      * Returns an XML object for the requested content, CMS page and OXID language ID
      *
@@ -163,17 +164,17 @@ class CmsConnect
      *
      * @return \SimpleXMLElement
      */
-    public function getContentXmlById ($sContentIdent = null, $sCmsPageId = null, $sLang = null)
+    public function getContentXmlById($sContentIdent = null, $sCmsPageId = null, $sLang = null)
     {
         $oCmsPage = new CmsPage\Id($sCmsPageId, $sLang);
-        
+
         return $oCmsPage->getContentXml($sContentIdent);
     }
-    
+
     /**
      * @deprecated
      */
-    public function getPageMetadata ($sField, $sCmsPagePath = null, $sLang = null)
+    public function getPageMetadata($sField, $sCmsPagePath = null, $sLang = null)
     {
         return $this->getMetadata($sField, $sCmsPagePath, $sLang);
     }
@@ -187,21 +188,21 @@ class CmsConnect
      *
      * @return string
      */
-    public function getMetadata ($sField, $sCmsPagePath = null, $sLang = null)
+    public function getMetadata($sField, $sCmsPagePath = null, $sLang = null)
     {
-        if ( $sCmsPagePath === null ) {
+        if ($sCmsPagePath === null) {
             $oCmsPage = new CmsPage\Path\Implicit($sLang);
         } else {
             $oCmsPage = new CmsPage\Path($sCmsPagePath, $sLang);
         }
-        
+
         return $oCmsPage->getMetadata($sField);
     }
-    
+
     /**
      * @deprecated
      */
-    public function getPageMetadataById ($sField, $sCmsPageId, $sLang = null)
+    public function getPageMetadataById($sField, $sCmsPageId, $sLang = null)
     {
         return $this->getMetadataById($sField, $sCmsPageId, $sLang);
     }
@@ -216,13 +217,13 @@ class CmsConnect
      *
      * @return string
      */
-    public function getMetadataById ($sField, $sCmsPageId, $sLang = null)
+    public function getMetadataById($sField, $sCmsPageId, $sLang = null)
     {
         $oCmsPage = new CmsPage\Id($sCmsPageId, $sLang);
-        
+
         return $oCmsPage->getMetadata($sField);
     }
-    
+
     /**
      * Alias of sanitizePageTitle()
      *
@@ -233,7 +234,7 @@ class CmsConnect
      *
      * @return string
      */
-    public function preparePagePath( $sUrl, $sLang = null )
+    public function preparePagePath($sUrl, $sLang = null)
     {
         return $this->sanitizePageTitle($sUrl, $sLang);
     }
@@ -244,11 +245,11 @@ class CmsConnect
      *
      * @return string
      */
-    public function sanitizePageTitle( $sUrl, $sLang = null )
+    public function sanitizePageTitle($sUrl, $sLang = null)
     {
-        return CMSc_Utils::sanitizePageTitle( $sUrl, $sLang );
+        return CMSc_Utils::sanitizePageTitle($sUrl, $sLang);
     }
-    
+
     /**
      * Public access to rewriteContentUrls helper function
      *
@@ -262,9 +263,9 @@ class CmsConnect
     }
     public function rewriteTextContentLinks($sContent)
     {
-        return CMSc_Utils::rewriteTextContentLinks( $sContent );
+        return CMSc_Utils::rewriteTextContentLinks($sContent);
     }
-    
+
     /**
      * Public access to rewriteUrl helper function
      *
@@ -274,93 +275,154 @@ class CmsConnect
      */
     public function rewriteUrl($sUrl)
     {
-        return CMSc_Utils::rewriteUrl( $sUrl );
+        return CMSc_Utils::rewriteUrl($sUrl);
     }
-    
+
     /**
      * TOXID compatibility function
      */
-    public function getXmlObject( $sPage = null, $sContentIdent = null )
+    public function getXmlObject($sPage = null, $sContentIdent = null)
     {
-        if ( $sContentIdent !== null ) {
-            return $this->getContentXml( $sContentIdent, $sPage );
+        if ($sContentIdent !== null) {
+            return $this->getContentXml($sContentIdent, $sPage);
         } else {
-            return $this->getXml( $sPage );
+            return $this->getXml($sPage);
         }
     }
-    
+
     /**
      * TOXID compatibility function
      */
-    public function getCmsSnippet( $sContentIdent, $sLang = null, $sPage = null )
+    public function getCmsSnippet($sContentIdent, $sLang = null, $sPage = null)
     {
         return $this->getCmsContentIdent($sContentIdent, $sLang, $sPage);
     }
-    
+
     /**
      * TOXID compatibility function
      */
-    public function getContentList( $sCustomPage = null, $blOnlyContentNodes = true )
+    public function getContentList($sCustomPage = null, $blOnlyContentNodes = true)
     {
         $aNodes = array();
-        
-        if ( $blOnlyContentNodes ) {
+
+        if ($blOnlyContentNodes) {
             // Default CMS column names
-            $aNodes = array(
-                'left',
-                'normal', 'content',
-                'right',
-                'border',
-            );
+            $aNodes = array('left', 'normal', 'content', 'right', 'border');
         }
-        
-        return $this->getContentArray( $sCustomPage, null, $aNodes );
+
+        return $this->getContentArray($sCustomPage, null, $aNodes);
     }
-    
+
     /**
      * TOXID compatibility function
      */
-    public function getCmsContentIdent( $sContentIdent = null, $blMultiLang = false, $sCustomPage = null )
-    {
-        return $this->getContent( $sContentIdent, $sCustomPage );
+    public function getCmsContentIdent(
+        $sContentIdent = null,
+        $blMultiLang = false,
+        $sCustomPage = null
+    ) {
+        return $this->getContent($sContentIdent, $sCustomPage);
     }
-    
+
     /**
      * TOXID compatibility function
      */
     public function toxidRewriteUrls($sContent, $iLangId = null, $blMultiLang = false)
     {
-        return CMSc_Utils::rewriteTextContentLinks( $sContent );
+        return CMSc_Utils::rewriteTextContentLinks($sContent);
     }
-    
+
     /**
      * TOXID compatibility function
      */
-    public function toxidRewriteUrl( $sUrl, $iLangId = null, $blMultiLang = false )
+    public function toxidRewriteUrl($sUrl, $iLangId = null, $blMultiLang = false)
     {
-        return CMSc_Utils::rewriteUrl( $sUrl );
+        return CMSc_Utils::rewriteUrl($sUrl);
     }
-    
+
     /**
      * TOXID compatibility function
      */
-    public function toxidEncodeTitle( $sUrl, $iLang = null )
+    public function toxidEncodeTitle($sUrl, $iLang = null)
     {
-        return $this->sanitizePageTitle( $sUrl, $iLang );
+        return $this->sanitizePageTitle($sUrl, $iLang);
     }
-    
+
     /**
      * TOXID compatibility function
      */
     public function getSearchResult($sKeywords)
     {
     }
-    
+
     /**
      * TOXID compatibility function
      */
-    public function getHttpCode( $sUrl = null )
+    public function getHttpCode($sUrl = null)
     {
         return 200;
+    }
+
+    protected static $_blCMScInitialized = false;
+
+    /**
+     * This method runs the cache initialization. It should be called from every user-facing API
+     * function to make sure the cache has been loaded before any requests are made.
+     * It is idempotent.
+     */
+    protected static function init()
+    {
+        if (!static::$_blCMScInitialized) {
+            static::$_blCMScInitialized = true;
+
+            Cache\LocalPages::get()->init();
+            Cache\CmsPages::get()->init();
+
+            $start = microtime(true);
+
+            $aCmsPages = Cache\LocalPages::get()->getCurrentLocalPageCmsPages();
+
+            $aPagesToFetch = [];
+
+            if (count($aCmsPages)) {
+                foreach ($aCmsPages as $oCmsPage) {
+                    $oHttpResult = Cache\CmsPages::get()->fetchHttpResult($oCmsPage);
+
+                    if ($oHttpResult) {
+                        SessionCache::set('results', $oCmsPage->getSessionCacheKey(), $oHttpResult);
+                    } else {
+                        $aPagesToFetch[] = $oCmsPage;
+                    }
+                }
+            }
+
+            // echo "<pre>Pages to fetch";
+            // var_dump($aPagesToFetch);
+            // echo "</pre>";
+
+            if (count($aPagesToFetch)) {
+                $aRequests = [];
+                foreach ($aPagesToFetch as $i => $oCmsPage) {
+                    $aRequests[] = $oCmsPage->getHttpRequest();
+                }
+
+                $aResults = CMSc_Utils::httpMultiRequest($aRequests);
+
+                // $aResults = [];
+                // foreach ( $aRequests as $i => $aRequest ) {
+                // list($aResult) = CMSc_Utils::httpMultiRequest([$aRequest]);
+                // $aResults[] = $aResult;
+                // }
+
+                $time = microtime(true) - $start;
+
+                // echo "<br>Time: " . $time . " s<br>";
+
+                foreach ($aPagesToFetch as $i => $oCmsPage) {
+                    SessionCache::set('results', $oCmsPage->getSessionCacheKey(), $aResults[$i]);
+                    Cache\CmsPages::get()->saveHttpResult($oCmsPage, $aResults[$i]);
+                }
+            }
+        }
     }
 }
